e36a313692900e89f9fd6028c79affe76d1f8676
shyam1819 <81102114+shyam1819@users.noreply.github.com>
2026-02-05 21:00:44 +0530

    Add function to find starting point of loop in linked list
    Implement algorithm to find the starting point of a loop in a linked list.

---
+++ Advanced/Linked-List/FAQs(Medium)/startingPointofLoop.py
@@ -0,0 +1,21 @@
+# Definition of singly linked list:
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def findStartingPoint(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        slow = head
+        while slow!=fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
