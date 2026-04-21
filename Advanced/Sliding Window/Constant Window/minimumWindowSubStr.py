ed38c3671411f6dddf8a0cd3d29d71563fdde113
shyam1819 <81102114+shyam1819@users.noreply.github.com>
2026-04-21 16:37:09 +0530

    Refactor minimumWindowSubStr for efficiency

--- Advanced/Sliding Window/Constant Window/minimumWindowSubStr.py
+++ Advanced/Sliding Window/Constant Window/minimumWindowSubStr.py
@@ -3,10 +3,12 @@ class Solution:
         #your code goes here
         freq = self.freqMap(t)
         fkeys = freq.keys()
+        required = len(t)
         i,j = 0,0
         n = len(s)
         m = {}
         substr = s
+        formed = 0
         has_matched=False
         if len(t)>len(s):
             return ""
@@ -14,22 +16,19 @@ class Solution:
             if s[j] in fkeys:
                 m[s[j]] = m.get(s[j],0)+1
                 # Loop for moving i
-                while self.validateFreq(freq,m):
+                if m[s[j]]<=freq[s[j]]:
+                    formed+=1
+                while required==formed:
                     has_matched=True
                     if j-i+1<len(substr):
                         substr = s[i:j+1]
                     if m.get(s[i],None):
-                        m[s[i]]=m[s[i]]-1  
+                        m[s[i]]=m[s[i]]-1 
+                        if m[s[i]]<freq[s[i]]:
+                            formed-=1
                     i+=1
             j+=1  
-        return substr if has_matched else ""  
-
-    def validateFreq(self,freq,m):
-        fmap = freq.keys()
-        for i in fmap:
-            if m.get(i,0)<freq[i]:
-                return False
-        return True 
+        return substr if has_matched else ""   
 
     def freqMap(self, s):
         freq = {}
